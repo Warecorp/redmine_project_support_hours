40bad3e91f9fd27d83a705e1bf55c16c1db59127
Ilya Andreyuk <aimilandr@gmail.com>
2014-06-09 15:55:33 +0300

    add project support hours json api

--- app/controllers/project_support_hours_controller.rb
+++ app/controllers/project_support_hours_controller.rb
@@ -4,6 +4,8 @@ class ProjectSupportHoursController < ApplicationController
 
   before_filter :require_admin
 
+  accept_api_auth :index
+
   def index
     @status = params[:status] ? params[:status].to_i : 1
     condition = @status == 0 ? ["status <> 0"] : ["status = ?", @status]
@@ -19,6 +21,7 @@ class ProjectSupportHoursController < ApplicationController
     respond_to do |format|
       format.html { render :action => "projects", :layout => false if request.xhr? }
       format.csv  { send_data(ProjectSupportHoursHelper.projects_to_csv(@projects), :type => 'text/csv; header=present', :filename => 'export.csv') }
+      format.json { render json: { projects_support_hours: ProjectSupportHoursHelper.projects_to_json(@projects) } }
     end
   end
 
